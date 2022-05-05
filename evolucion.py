b730df91f30bb1df590754e69b193574051708a7
Juan Tomás <73398334+juantomasmoreno@users.noreply.github.com>
2022-05-05 09:17:49 +0200

    Update evolucion.py

--- evolucion.py
+++ evolucion.py
@@ -63,13 +63,14 @@ class Clase:
                     f'Directorio actual:{os.getcwd()}')
    
     def buscar(self):
-        texto = st.text_input('¿A quién buscas?', '', key=1).lower()
+        texto = ''
+        texto = st.text_input('¿A quién buscas?', input('¿A quién buscas? '), key=1).lower()
         n = 1
         i = 2
         dic = {}
         while len(texto) < 3 or not dic:
             st.caption('Introduce más carácteres en la búsqueda')
-            texto = st.text_input('¿A quién buscas?', '', key=i).lower()
+            texto = st.text_input('¿A quién buscas?', input('¿A quién buscas? '), key=i).lower()
             i += 1
         for ide,jugador in self.jugadores.items():
             if texto in jugador.nombre.lower().split(): 
