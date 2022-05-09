0b0c801bb7991245cfd736f61aec027c1fecab16
Juan Tomás <73398334+juantomasmoreno@users.noreply.github.com>
2022-05-09 10:08:59 +0200

    Update posicion.py

--- posicion.py
+++ posicion.py
@@ -6,6 +6,8 @@ Created on Tue Apr 12 08:47:20 2022
 """
 import csv
 import os
+import streamlit as st
+
 class Jugador22:
     def __init__(self,ide, nombre,posicion,media,potencial,edad,altura,club,liga,nivelLiga,pais,pDom,nCorto):      
         self.ide = ide
@@ -21,6 +23,7 @@ class Jugador22:
         self.pais= pais
         self.pDom=pDom
         self.nCorto=nCorto
+        
 class Clase:
     def __init__(self):
         self.jugadores = {}
@@ -31,8 +34,6 @@ class Clase:
             self.jugadores[ide] = Jugador22(ide,nombre ,posicion,media,potencial,edad,altura,club,liga,nivelLiga,pais,pDom,nCorto)
             return True, 'Añadido correctamente'
         else:
-    
-        
             self.jugadores[ide] = Jugador22(ide,nombre ,posicion,media,potencial,edad,altura,club,liga,nivelLiga,pais,pDom,nCorto)
             return True, 'Añadido correctamente'
     
@@ -44,10 +45,8 @@ class Clase:
                 next(reader)
                 for sofifa_id,long_name,player_positions,overall,potential,age,height_cm,club_name,league_name,league_level,nationality_name,preferred_foot,short_name in reader:
                     b, msg = self.nuevo_jugador(sofifa_id,long_name,player_positions,int(overall),potential,age,height_cm,club_name,league_name,league_level,nationality_name,preferred_foot,short_name)
-                    
                     if b:                 
                         leidos += 1
-                    
                 print ( f'{leidos} jugadores leidos correctamente')
                 return True, 'correcto'
         except Exception as ex:
@@ -55,13 +54,10 @@ class Clase:
                     f'{type(ex).__name__}\nArgumentos:{ex.args}. \n'+
                     f'Directorio actual:{os.getcwd()}')
 
-
-        
     def buscar_nac(self):
         texto = ''
         n = 1
-        dic = {}
-            
+        dic = {}   
         for ide,jugador in self.jugadores.items():
             if texto in jugador.pais.lower() and jugador.pais not in dic.values(): 
                 print(f'{n} {jugador.pais}')
@@ -82,7 +78,6 @@ class Clase:
         texto = ''
         n = 1
         dic = {}
-        
         for ide,jugador in self.jugadores.items():
             lista=jugador.posicion.lower().split(',')
             for i in lista:
@@ -100,6 +95,7 @@ class Clase:
                 return texto        
             return dic[elec]
         return texto
+    
     def buscar_liga(self):
         texto = ''
         n = 1
@@ -122,7 +118,6 @@ class Clase:
         return texto
     
     def jugadores_pos_nat(self,nat,pos,liga):
-        
         lista=[]
         for ide,jugador in self.jugadores.items():
             if nat==jugador.pais and liga==jugador.liga and pos.lower() in jugador.posicion.lower().split(",") and len(lista)<11:
@@ -137,15 +132,16 @@ class Clase:
                 lista.append((jugador.nombre,jugador.media))
             elif nat=='' and liga==jugador.liga and pos=='' and len(lista)<11:
                 lista.append((jugador.nombre,jugador.media))
+        for i in lista:
+            cadena = f'{i[0]}: {i[1]}'
+            st.subheader(cadena)
             
-        print(lista)
-            
-
-
-
-c=Clase()
-c.cargar_datos('Fifa22.csv') 
-nat=c.buscar_nac()
-pos=c.buscar_pos()
-liga=c.buscar_liga()
-c.jugadores_pos_nat(nat,pos,liga)
+if __name__ == '__main__':
+    c=Clase()
+    c.cargar_datos('Fifa22.csv')
+    st.title('FIFAFURBO!')
+    st.header('Aquí podrás filtrar los mejores jugadores por liga, nacionalidad o posición.')
+    nat=c.buscar_nac()
+    pos=c.buscar_pos()
+    liga=c.buscar_liga()
+    c.jugadores_pos_nat(nat,pos,liga)
