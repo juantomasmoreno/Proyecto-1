f60d7199a34246956e9374bccb7c14ced2f1afe8
Juan Tomás <73398334+juantomasmoreno@users.noreply.github.com>
2022-05-12 10:41:59 +0200

    Add files via upload

---
+++ clubs.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 11 10:23:33 2022
+
+@author: adrir
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+df=pd.read_csv('Career_Mode_FIFA.csv' , delimiter= ';')
+df.head()
+liga=input('Que liga?')
+fifa=int(input('Que fifa?'))
+df1=df.loc[(df['league_name'] == liga) & (df['FIFA']==fifa)]
+
+x=df1.groupby('club_name')['overall'].mean()+df1.groupby('club_name')['overall'].median()-135
+y = x.sort_values(ascending=True)
+plt.figure(figsize=(10,20))
+plt.grid(axis='x')
+plt.title(f'Valoracion media clubs {liga}  fifa {fifa}')
+plt.barh(y.index, y.values)
+plt.show()
