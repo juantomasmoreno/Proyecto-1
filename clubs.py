360bd61d2c87e3e3e1649cb144953b9e735d1b6a
Juan Tomás <73398334+juantomasmoreno@users.noreply.github.com>
2022-05-12 10:57:08 +0200

    Update clubs.py

--- clubs.py
+++ clubs.py
@@ -10,13 +10,13 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 st.image('header.png')
-df=pd.read_csv('Career_Mode_FIFA.csv' , delimiter= ';')
-df.head()
-liga=input('Que liga?')
-fifa=st.slider('¿Qué FIFA quieres comprobar?', 15, 22, 15, 1)
-df1=df.loc[(df['league_name'] == liga) & (df['FIFA']==fifa)]
+df = pd.read_csv('Career_Mode_FIFA.csv' , delimiter= ';')
+liga = st.selectbox('How would you like to be contacted?',
+                   ('English Premier League', 'Spain Primera Division', 'Italian Serie A', 'German 1. Bundesliga', 'French Ligue 1'), 0)
+fifa = st.slider('¿Qué FIFA quieres comprobar?', 15, 22, 15, 1)
+df1 = df.loc[(df['league_name'] == liga) & (df['FIFA']==fifa)]
 
-x=df1.groupby('club_name')['overall'].mean()+df1.groupby('club_name')['overall'].median()-135
+x = df1.groupby('club_name')['overall'].mean()+df1.groupby('club_name')['overall'].median()-135
 y = x.sort_values(ascending=True)
 plt.figure(figsize=(10,20))
 plt.grid(axis='x')
