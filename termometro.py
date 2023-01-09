79fd419ae29dbe54fcb238730ecc5d25be5f02fc
Juan Tomás <73398334+juantomasmoreno@users.noreply.github.com>
2023-01-09 19:07:48 +0100

    Add files via upload

--- termometro.py
+++ termometro.py
@@ -1,28 +1,25 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 12 08:47:20 2022
 
-@author: adrir
-"""
 import csv
 import os
-import streamlit as st
+
 
 class Grado:
-    def __init__(self, id, nombre, uni, uni_a, nota, plazas):
-        self.id = int(id)
+    def __init__(self, ide, nombre, uni, uni_a, nota, plazas):
+        self.id = int(ide)
         self.nombre = nombre
         self.uni = uni
         self.uni_a = uni_a
-        self.nota = int(nota)
+        self.nota = float(nota)
         self.plazas = int(plazas)
+        
         
 class Clase:
     def __init__(self):
         self.carreras = {}
         
-    def nueva_carrera(self, nombre, uni_a, nota, plazas):
-        self.carreras[id] = Grado(id, nombre, uni_a, nota, plazas)
+    def nueva_carrera(self,ide, nombre,uni, uni_a, nota, plazas):
+        self.carreras[ide] = Grado(ide, nombre,uni, uni_a, nota, plazas)
         return True, 'Añadido correctamente'
     
     def cargar_datos(self, fichero):
@@ -31,8 +28,9 @@ class Clase:
                 r = csv.reader(csvfile, delimiter=';')
                 leidos = 0
                 next(r)
-                for id, nombre, uni, uni_a, nota, plazas in r:
-                    b, msg = self.nueva_carrera(nombre, uni, uni_a, nota, plazas)
+                for ide, nombre, uni, uni_a, nota, plazas in r:
+                    nota=nota.replace(',','.',1)
+                    b, msg = self.nueva_carrera(ide,str(nombre), str(uni), uni_a, nota, plazas)
                     if b:                 
                         leidos += 1
                 print(f'{leidos} carreras leídas correctamente')
@@ -42,22 +40,41 @@ class Clase:
                     f'{type(ex).__name__}\nArgumentos:{ex.args}. \n'+
                     f'Directorio actual:{os.getcwd()}')
 
-    
+def buscar_uni(clase):
+    texto = ''
+    texto = input('Elige que grado quieres buscar:').lower()
+    n = 1
+    dic = {}
+    if len(texto) < 3:
+        print('Introduce más carácteres en la búsqueda')
+    else:
+        for grado in clase.carreras.values():
+            if texto in grado.nombre.lower():
+                print(f'{n} {grado.nombre} ({grado.uni})')
+                dic[n] = grado
+                n += 1
         
-    def buscar_uni(self):
-        texto = ''
-        texto = st.text_input('Elige en qué universidad quieres buscar','', key=2).lower()
-        n = 1
-        dic = {}      
-        return texto
-    
-    
-    
-            
+        if not dic:
+            print('No hay ningún alumno con esa búsqueda')
+        else:    
+            elec = 0
+            if elec < 1 or elec > len(dic):
+                 try:
+                     elec = int(input('Elige una universidad: '))
+                     grado=dic[elec]
+                     print(f'{grado.nombre},{grado.uni},{grado.uni_a},{grado.nota},{grado.plazas}')
+                     
+                 except:
+                     print('Vuelve a elegir')
+                     
+                 
+        
+  
+           
 if __name__ == '__main__':
     c=Clase()
     c.cargar_datos('tprueba.csv')
     # st.image('header.png')
-    st.header('Aquí podrás encontrar tu próxima carrera universitaria.')
-    uni = c.buscar_uni()
-    st.caption(uni)
+    print('Aquí podrás encontrar tu próxima carrera universitaria.')
+    uni = buscar_uni(c)
+    
